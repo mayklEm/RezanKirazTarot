a280e55055f6ecb07e38ae11c76e6a2cfae477b5
mayklEm <matik.peter@gmail.com>
2014-06-24 14:27:34 +0300

    v 1.01 - added activity for card detail

---
+++ app/src/main/java/done/rezankiraztarot/app/DetailActivity.java
@@ -0,0 +1,15 @@
+package done.rezankiraztarot.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Peter on 24.6.2014.
+ */
+public class DetailActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_layout);
+    }
+}
