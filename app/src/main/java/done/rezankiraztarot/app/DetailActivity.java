49084fe2741608a5a8d394cc9368b37cd5103bd3
mayklEm <matik.peter@gmail.com>
2014-06-24 18:08:07 +0300

    - added zoom out animation for big image

--- app/src/main/java/done/rezankiraztarot/app/DetailActivity.java
+++ app/src/main/java/done/rezankiraztarot/app/DetailActivity.java
@@ -3,15 +3,49 @@ package done.rezankiraztarot.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Peter on 24.6.2014.
  */
-public class DetailActivity extends Activity {
+public class DetailActivity extends Activity implements Animation.AnimationListener {
+    Animation animZoomOut;
+    ImageView bigImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.card_layout);
+
+        bigImage = (ImageView) findViewById(R.id.big_image);
+
+        animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_out);
+        // set animation listener
+        animZoomOut.setAnimationListener(this);
+        bigImage.startAnimation(animZoomOut);
     }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
